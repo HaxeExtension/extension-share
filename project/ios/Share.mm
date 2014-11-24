b18a139f56a94710e5347f926be6fa374d33366a
Federico Bricker <fbricker@gmail.com>
2014-11-24 01:31:09 -0300

    Fixed support for iPad.

--- project/ios/Share.mm
+++ project/ios/Share.mm
@@ -13,10 +13,20 @@ namespace openflShareExtension {
         }else{
 	        itemsToShare = @[sText];        	
         }
-        UIActivityViewController  *activityVC = [[UIActivityViewController alloc] initWithActivityItems:itemsToShare applicationActivities:nil];
+        UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:itemsToShare applicationActivities:nil];
         if(subject != nil){
             [activityVC setValue:[[NSString alloc] initWithUTF8String:subject] forKey:@"subject"];
         }
+
+        // Required for iPad
+        activityVC.popoverPresentationController.sourceView = [[UIApplication sharedApplication] keyWindow];
+
+        // Remove arrow from action sheet.
+        [activityVC.popoverPresentationController setPermittedArrowDirections:0];
+
+        // Set action sheet to middle of view.
+        activityVC.popoverPresentationController.sourceRect = [[UIApplication sharedApplication] keyWindow].frame;
+
         activityVC.excludedActivityTypes = @[UIActivityTypeAddToReadingList,
                                              UIActivityTypeCopyToPasteboard,
                                              UIActivityTypePrint,
