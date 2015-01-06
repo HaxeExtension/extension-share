917584cd5916d94f69e9f5f1b640d121cf403a30
Federico Bricker <fbricker@gmail.com>
2015-01-06 00:19:48 -0300

    Fix build for arm64, x86_64.

--- buildPackage.sh
+++ buildPackage.sh
@@ -3,4 +3,6 @@ dir=`dirname "$0"`
 cd "$dir"
 rm -f openfl-share.zip
 rm -rf project/obj
+lime rebuild . ios
+rm -rf project/obj
 zip -0r openfl-share.zip extension haxelib.json include.xml java ndll project
--- project/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dir=`dirname "$0"`
-cd "$dir"
-
-haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARMV7
-haxelib run hxcpp Build.xml -Diphonesim
