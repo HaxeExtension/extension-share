48a597e3537b4187393e1ecc5de5367fd833b88f
Federico Bricker <fbricker@gmail.com>
2015-09-21 14:36:45 -0300

    Enable support for lime next (use Extension.mainActivity instead of GameActivity.getInstance()).

--- java/ShareEx/project/android/shareex/ShareEx.java
+++ java/ShareEx/project/android/shareex/ShareEx.java
@@ -1,19 +1,19 @@
-
 package shareex;
 
 import android.content.Intent;
-import org.haxe.lime.GameActivity;
+import org.haxe.extension.Extension;
 
 public class ShareEx {
-    
+
 	public static void share(String text, String subject, String html, String email) {
 		Intent sendIntent = new Intent(android.content.Intent.ACTION_SEND);
+		sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		sendIntent.setType("text/plain");
-        if(subject!=null && subject!="")	sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
-        if(text!=null && text!="")	sendIntent.putExtra(android.content.Intent.EXTRA_TEXT, text);
-        if(html!=null && html!="")	sendIntent.putExtra(android.content.Intent.EXTRA_HTML_TEXT, html);
+		if(subject!=null && subject!="") sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+		if(text!=null && text!="") sendIntent.putExtra(android.content.Intent.EXTRA_TEXT, text);
+		if(html!=null && html!="") sendIntent.putExtra(android.content.Intent.EXTRA_HTML_TEXT, html);
 		if(email!=null && email!="") sendIntent.putExtra(android.content.Intent.EXTRA_EMAIL, email);
-        GameActivity.getInstance().getContext().startActivity(sendIntent);
+		Extension.mainActivity.getApplicationContext().startActivity(sendIntent);
 	}
 
 }
